68de3d4e1faec090b7b78caf3a9a618559f0a3fe
lyb0623 <nnoonxnoonn@gmail.com>
2022-10-27 10:56:19 +0900

    수정

--- src/main/java/com/plannet/servlet/MemberServlet.java
+++ src/main/java/com/plannet/servlet/MemberServlet.java
@@ -1,11 +1,23 @@
 package com.plannet.servlet;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.List;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+
+import com.plannet.common.Common;
+import com.plannet.dao.MemberDAO;
+import com.plannet.vo.MemberVO;
 
 @WebServlet("/MemberServlet")
 public class MemberServlet extends HttpServlet {
@@ -16,10 +28,55 @@ public class MemberServlet extends HttpServlet {
     }
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
 		response.getWriter().append("Served at: ").append(request.getContextPath());
 	}
+	protected void doOptions(HttpServletRequest request, HttpServletResponse response)
+		    throws ServletException, IOException {
+		response = Common.corsResSet(response);
+	}
 
+	@SuppressWarnings("unchecked")
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		doGet(request, response);
+		request.setCharacterEncoding("utf-8");
+		response = Common.corsResSet(response);
+
+		StringBuffer sb = Common.reqStringBuff(request);
+		JSONObject jsonObj = Common.getJsonObj(sb);
+		
+		System.out.println("Command : " + (String)jsonObj.get("cmd"));
+		String reqCmd = (String)jsonObj.get("cmd");
+		
+		PrintWriter out = response.getWriter();
+		
+		if(!reqCmd.equals("MemberInfo")) {
+			JSONObject resJson = new JSONObject();
+			resJson.put("result", "NOK");
+			out.print(resJson);
+			return;
+		}
+		
+		MemberDAO dao = new MemberDAO();
+		List<MemberVO> list = dao.memberSelect();
+		
+		JSONArray memberArray = new JSONArray();
+		
+		for(MemberVO e : list) {
+			JSONObject memberInfo = new JSONObject();
+			memberInfo.put("id", e.getId());
+			memberInfo.put("pwd", e.getPwd());
+			memberInfo.put("name", e.getName());
+			memberInfo.put("email", e.getEmail());
+			DateFormat dateFormat = new SimpleDateFormat("YYYY/MM/dd HH:mm:ss");
+			String dateToStr = dateFormat.format(e.getJoin());
+			memberInfo.put("join", dateToStr);
+			memberArray.add(memberInfo);
+		}	
+		out.print(memberArray);
+	}
+	
+	@Override
+	public void destroy() {
+		System.out.println("destroy 메소드 호출");
 	}
 }
--- src/main/java/com/plannet/vo/MemberVO.java
+++ src/main/java/com/plannet/vo/MemberVO.java
@@ -1,5 +1,65 @@
 package com.plannet.vo;
 
-public class MemberVO {
+import java.sql.Date;
 
+public class MemberVO {
+	private String id;
+	private String pwd;
+	private String name;
+	private String nickname;
+	private String email;
+	private String tel;
+	private Date birth;
+	private Date join;
+	
+	
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getPwd() {
+		return pwd;
+	}
+	public void setPwd(String pwd) {
+		this.pwd = pwd;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getNickname() {
+		return nickname;
+	}
+	public void setNickname(String nickname) {
+		this.nickname = nickname;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getTel() {
+		return tel;
+	}
+	public void setTel(String tel) {
+		this.tel = tel;
+	}
+	public Date getBirth() {
+		return birth;
+	}
+	public void setBirth(Date birth) {
+		this.birth = birth;
+	}
+	public Date getJoin() {
+		return join;
+	}
+	public void setJoin(Date join) {
+		this.join = join;
+	}
 }
+
