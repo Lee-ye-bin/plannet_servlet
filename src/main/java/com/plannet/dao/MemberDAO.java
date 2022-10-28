9a16e97b7cecd1c0a258d4be56116218b4bbf519
ksh0918 <kim.sehee.0918@gmail.com>
2022-10-28 14:51:28 +0900

    Merge branch 'master' of https://github.com/Lee-ye-bin/plannet_servlet.git

--- src/main/java/com/plannet/dao/MemberDAO.java
+++ src/main/java/com/plannet/dao/MemberDAO.java
@@ -1,6 +1,5 @@
 package com.plannet.dao;
 
-import java.io.Console;
 import java.sql.Connection;
 import java.sql.Date;
 import java.sql.PreparedStatement;
@@ -17,6 +16,9 @@ public class MemberDAO {
 	private Statement stmt = null; //표준 SQL문을 수행하기 위한 Statement 객체 얻기
 	private ResultSet rs = null; // Statement의 수행 결과를 여러행으로 받음
 	// SQL문을 미리 컴파일해서 재 사용하므로 Statement 인터페이스보다 훨씬 빨르게 데이터베이스 작업을 수행
+	
+	
+	//test
 	private PreparedStatement pstmt = null; 
 	
 	public boolean logingCheck(String id, String pwd) {
@@ -25,7 +27,6 @@ public class MemberDAO {
 			stmt = conn.createStatement(); // Statement 객체 얻기
 			String sql = "SELECT * FROM MEMBER WHERE ID = " + "'" + id + "'";
 			rs = stmt.executeQuery(sql);
-			
 			while(rs.next()) { // 읽은 데이타가 있으면 true
 				String sqlId = rs.getString("ID"); // 쿼리문 수행 결과에서 ID값을 가져옴
 				String sqlPwd = rs.getString("PWD"); // 쿼리문 수행 결과에서 PWD값을 가져옴
@@ -107,6 +108,7 @@ public class MemberDAO {
 			}
 			
 			rs = stmt.executeQuery(sql);
+			System.out.println(rs);
 			
 			if(rs.next()) isNotReg = false;
 			else isNotReg = true;
