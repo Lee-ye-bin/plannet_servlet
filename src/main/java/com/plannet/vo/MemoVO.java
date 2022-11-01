699e4f287d06db218c5a019c8fba67098d5a8307
ksh0918 <kim.sehee.0918@gmail.com>
2022-11-01 11:03:11 +0900

    memovo수정

--- src/main/java/com/plannet/vo/MemoVO.java
+++ src/main/java/com/plannet/vo/MemoVO.java
@@ -1,37 +1,21 @@
 package com.plannet.vo;
 
-import java.sql.Date;
-import java.util.List;
-
 public class MemoVO {
 	String id;
-	Date date;
-	List<String> plan;
-	String diary;
+	String memo;
 	
+	public String getMemo() {
+		return memo;
+	}
+	public void setMemo(String memo) {
+		this.memo = memo;
+	}
 	public String getId() {
 		return id;
 	}
 	public void setId(String id) {
 		this.id = id;
 	}
-	public Date getDate() {
-		return date;
-	}
-	public void setDate(Date date) {
-		this.date = date;
-	}
-	public List<String> getPlan() {
-		return plan;
-	}
-	public void setPlan(List<String> plan) {
-		this.plan = plan;
-	}
-	public String getDiary() {
-		return diary;
-	}
-	public void setDiary(String diary) {
-		this.diary = diary;
-	}
+
 
 }
