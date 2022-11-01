05be2437c2c9c0d0ff2102bc6ca24594a819b68f
ksh0918 <kim.sehee.0918@gmail.com>
2022-11-01 16:31:58 +0900

    write 화면 구현

--- src/main/java/com/plannet/dao/WriteDAO.java
+++ src/main/java/com/plannet/dao/WriteDAO.java
@@ -6,47 +6,81 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.Statement;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 
 import com.plannet.common.Common;
 import com.plannet.vo.MemberVO;
 import com.plannet.vo.MemoVO;
+import com.plannet.vo.WriteVO;
 
 public class WriteDAO {
 	private Connection conn = null;
 	private Statement stmt = null; //표준 SQL문을 수행하기 위한 Statement 객체 얻기
 	private ResultSet rs = null; // Statement의 수행 결과를 여러행으로 받음
+
 	// SQL문을 미리 컴파일해서 재 사용하므로 Statement 인터페이스보다 훨씬 빨르게 데이터베이스 작업을 수행
 	private PreparedStatement pstmt = null; 
 
 	
-//	public  List<MemoVO> memberMemo(String id) {
-//		List<MemoVO> list = new ArrayList<>();
-//		
-//		try {
-//			conn = Common.getConnection();
-//			stmt = conn.createStatement(); // Statement 객체 얻기
-//			String sql = "SELECT * FROM MEMO WHERE ID = " + "'" + id + "'";
-//			rs = stmt.executeQuery(sql);
-//			
-//			while(rs.next()) { // 읽은 데이타가 있으면 true
-//				String sqlMemo = rs.getString("MEMO"); // 쿼리문 수행 결과에서 ID값을 가져옴
-//				String sqlId = rs.getString("ID");
-//				System.out.println("MEMO : " + sqlMemo);
-//				MemoVO vo = new MemoVO();
-//				vo.setId(sqlId);
-//				vo.setMemo(sqlMemo);
-//				list.add(vo);
-//			}
-//			Common.close(rs);
-//			Common.close(stmt);
-//			Common.close(conn);
-//		} catch(Exception e) {
-//			e.printStackTrace();
-//		}
-//		return list;
-//	}
+	public  List<WriteVO> writeLoad(String id, Date date) {
+		List<WriteVO> list = new ArrayList<>();
+		
+		try {
+			//리스트 가져오기
+			conn = Common.getConnection();
+			stmt = conn.createStatement(); // Statement 객체 얻기
+			
+			String sqlListLoad = "SELECT * FROM PLAN WHERE ID = " + "'" + id + "'" + " AND PLAN_DATE = " + "'" + date + "' ORDER BY PLAN_NO ASC";
+			rs = stmt.executeQuery(sqlListLoad);
+			
+			WriteVO vo = new WriteVO();
+			
+			List<Map<String, Object>> planList = new ArrayList<Map<String, Object>>();
+			while(rs.next()) { // 읽은 데이타가 있으면 true
+				Map<String, Object> planItem = new LinkedHashMap<String, Object>();
+				
+				planItem.put("id", rs.getString("PLAN_NO"));
+				if(rs.getInt("PLAN_CHECK") == 1) planItem.put("checked", true);
+				else planItem.put("checked", false);
+				planItem.put("text", rs.getString("PLAN"));
+				planItem.put("deleted", false);
+				
+				planList.add(planItem);
+			}
+			vo.setPlan(planList);
+			
+			Common.close(rs);
+			Common.close(stmt);
+			Common.close(conn);
+			
+			
+			
+			//다이어리 가져오기
+			conn = Common.getConnection();
+			stmt = conn.createStatement();
+			
+			String sqlDiaryLoad = "SELECT * FROM DIARY WHERE ID = "+ "'" + id + "'" + " AND DIARY_DATE = " + "'" + date +"'";
+			rs = stmt.executeQuery(sqlDiaryLoad);
+			while(rs.next()) {
+				String sqlDiary = rs.getString("DIARY");
+				vo.setDiary(sqlDiary);
+			}
+			
+			Common.close(rs);
+			Common.close(stmt);
+			Common.close(conn);
+    		//리스트에 담기
+			list.add(vo);
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+		
+		return list;
+	}
 //	
 	public void writeSave(String id, Date date, List<Map<String, Object>> plan, String diary) {
 		String sqlDiaryCheck = "SELECT * FROM DIARY WHERE ID = '" + id + "' AND DIARY_DATE = '" + date + "'" ;
@@ -64,12 +98,12 @@ public class WriteDAO {
 	    	pstmt.executeUpdate();
 	    	
 	    	//PLAN 저장
-	    	int cnt = 1;
+	    	int cnt = 0;
 	    	
 	    	for(Map<String, Object> p : plan) {
-	    		String deleted = (String) p.get("deleted");
+	    		boolean deleted = (boolean) p.get("deleted");
 	    		//System.out.println(p.get("deleted"));
-	    		if(deleted.equals("false")) {
+	    		if(deleted == false) {
 	    			pstmt = conn.prepareStatement(sqlListInsert); // 미리 만들어둔 쿼리문 양식에 맞춰 넣음
 	    	    	pstmt.setString(1, id);
 	    	    	pstmt.setDate(2, date);
