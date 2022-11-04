7a47790ef0a435a3bf37a8132c09c58aa01ddbac
chan0823 <a3327764@naver.com>
2022-11-04 17:29:51 +0900

    Merge branch 'master' of https://github.com/Lee-ye-bin/plannet_servlet.git

--- src/main/java/com/plannet/servlet/BoardLoad.java
+++ src/main/java/com/plannet/servlet/BoardLoad.java
@@ -34,7 +34,7 @@ public class BoardLoad extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		response.getWriter().append("Served at: ").append(request.getContextPath());
 	}
-
+//
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) 
 			throws ServletException, IOException {
 		request.setCharacterEncoding("utf-8");
